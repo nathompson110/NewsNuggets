26dbe12ab67356b18b1dae1a05233a31499ffe1a
Natalie <nathompson110@gmail.com>
2017-09-24 21:35:54 -0500

    Set up files

---
+++ server.js
@@ -0,0 +1,11 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
